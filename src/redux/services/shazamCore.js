76d6ffbf408f01a77c9e83150e131150f995cb14
Artur Nunes <arturantoniojj@gmail.com>
2022-09-27 17:34:11 -0300

    Criação da estrutura do projeto

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '8aac56c862mshe6ff6cc67b0d400p1aa595jsn0abd092a31fe');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
